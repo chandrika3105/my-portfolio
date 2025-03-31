afd688cca982022f930c8becc16980a49c6723ed
Chandrika Sangineedi <chandrikaa3105@gmail.com>
2025-03-31 20:07:52 +0530

    My-portfolio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,38 @@
+// import { NgModule } from '@angular/core';
+// import { ExtraOptions, RouterModule, Routes } from '@angular/router';
+
+// const routes: Routes = [];
+
+// const routerOptions: ExtraOptions = {
+//   anchorScrolling: 'enabled',
+//   scrollPositionRestoration: 'enabled',
+// };
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes,routerOptions)],
+//   exports: [RouterModule]
+// })
+
+// export class AppRoutingModule { }
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent }, // Keep HomeComponent as your landing page
+  { path: 'about', component: AboutComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, { 
+      scrollPositionRestoration: 'enabled', 
+      anchorScrolling: 'enabled' 
+    })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SkillsComponent } from './skills/skills.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
+import { FormsModule } from '@angular/forms';
+import { ExperienceComponent } from './experience/experience.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    FooterComponent,
+    ContactComponent,
+    ExperienceComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
